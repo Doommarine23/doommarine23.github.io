6ae0e344944687ec9f84df4dc67ea1d60b67281d
Doommarine23 <doom.marine23@gmail.com>
2018-01-24 14:50:54 -0500

    Initial commit

---
+++ media/menu.js
@@ -0,0 +1,12 @@
+//Used for dropdown menu.
+function openMenu() {
+	menuicon.style.color = "black";
+    console.log("aloha");
+    menubox.style.display = "inline";
+}
+
+function closeMenu() {
+	menuicon.style.color = "white";
+    console.log("goodbye");
+    menubox.style.display = "none";
+}
